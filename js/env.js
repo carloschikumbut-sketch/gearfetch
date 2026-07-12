3b994261cdc6afd31192f9fec06145f627a75011
carloschikumbut-sketch <carloschikumbut@gmail.com>
2026-07-12 20:53:53 +0200

    Add ENV configuration for Supabase

---
+++ js/env.js
@@ -0,0 +1,4 @@
+window.ENV = {
+    SUPABASE_URL: "https://bbzdwxrljjdabynquqvm.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiemR3eHJsampkYWJ5bnF1cXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODU0MDgsImV4cCI6MjA4NzM2MTQwOH0.vJgV1_mYDAGoZs-PfvjCu3EWPXmkoCX0b_Xs8c7qISQ"
+};
